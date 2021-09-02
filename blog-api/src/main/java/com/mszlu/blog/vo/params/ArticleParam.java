beb99afc0dde566887cb7206b31943703dad4e01
liuzidi <liuzidi17@163.com>
2021-09-02 11:08:50 +0800

    commit my project

---
+++ blog-api/src/main/java/com/mszlu/blog/vo/params/ArticleParam.java
@@ -0,0 +1,18 @@
+package com.mszlu.blog.vo.params;
+
+import com.mszlu.blog.vo.CategoryVo;
+import com.mszlu.blog.vo.TagVo;
+import lombok.Data;
+
+import java.util.List;
+
+//文章参数
+@Data
+public class ArticleParam {
+    private Long id;
+    private ArticleBodyParam body;
+    private CategoryVo category;
+    private String summary;
+    private List<TagVo> tags;
+    private String title;
+}
